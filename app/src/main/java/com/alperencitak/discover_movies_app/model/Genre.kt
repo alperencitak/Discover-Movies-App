66da8dbc4435edb2b1ac98086870e566dfbca234
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-05 13:39:32 +0300

    Genre model classes written

---
+++ app/src/main/java/com/alperencitak/discover_movies_app/model/Genre.kt
@@ -0,0 +1,6 @@
+package com.alperencitak.discover_movies_app.model
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/alperencitak/discover_movies_app/model/GenreResponse.kt
@@ -0,0 +1,5 @@
+package com.alperencitak.discover_movies_app.model
+
+data class GenreResponse(
+    val genres: List<Genre>
+)
