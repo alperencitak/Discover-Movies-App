469fa116b086623640d7100a32f3e790de0bcf25
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-05 12:54:54 +0300

    Update Manifest

--- app/src/main/java/com/alperencitak/discover_movies_app/ui/theme/Theme.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.alperencitak.discover_movies_app.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
