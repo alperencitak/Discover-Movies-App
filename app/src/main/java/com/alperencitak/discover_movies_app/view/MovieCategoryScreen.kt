76dfa9f7c171accae58171302b9d9afb0dd7f452
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-05 23:04:28 +0300

    Category page written

---
+++ app/src/main/java/com/alperencitak/discover_movies_app/view/MovieCategoryScreen.kt
@@ -0,0 +1,95 @@
+package com.alperencitak.discover_movies_app.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
+import com.alperencitak.discover_movies_app.model.Movie
+import com.alperencitak.discover_movies_app.ui.theme.SoftBlack
+import com.alperencitak.discover_movies_app.ui.theme.SoftRed
+import com.alperencitak.discover_movies_app.viewmodel.MovieViewModel
+
+@Composable
+fun MovieCategoryScreen() {
+
+    val moviesViewModel: MovieViewModel = hiltViewModel()
+    val moviesByGenre by moviesViewModel.moviesByGenre.collectAsState()
+    val genres by moviesViewModel.genres.collectAsState()
+    val genreList = listOf(27, 28, 12, 16, 35, 80, 18, 10751, 14, 9648, 10749, 878, 10752, 53)
+
+    moviesViewModel.getMovieGenres()
+    genreList.forEach { genreId ->
+        moviesViewModel.getMoviesByGenre(1, genreId)
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize().background(SoftBlack)
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(top = 64.dp).verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center
+    ) {
+        genreList.forEach { genreId ->
+            val movies = moviesByGenre[genreId] ?: emptyList()
+            val genreName = genres.find{ it.id == genreId}?.name ?: ""
+            ListRow(genreName, movies)
+        }
+    }
+
+}
+
+@Composable
+fun ListRow(title: String, movies: List<Movie>){
+    Column(
+        modifier = Modifier.fillMaxWidth().height(300.dp).padding(horizontal = 16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = title, fontWeight = FontWeight.Bold, fontSize = 18.sp, color = SoftRed)
+            Text(text = "See All", fontWeight = FontWeight.Bold, fontSize = 20.sp, color = SoftRed)
+        }
+        LazyRow (
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            items(movies){ movie ->
+                ElevatedCard(
+                    modifier = Modifier.fillMaxHeight().width(200.dp).padding(horizontal = 4.dp, vertical = 8.dp)
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(model = movie.getFullPosterUrl()),
+                        contentScale = ContentScale.Crop,
+                        contentDescription = "Movie Poster",
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+            }
+        }
+    }
+}
