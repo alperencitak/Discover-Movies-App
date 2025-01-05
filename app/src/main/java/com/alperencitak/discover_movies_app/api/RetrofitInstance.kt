f48fc27353240a289339256eb8221d6ef8538509
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-05 13:39:21 +0300

    Add filters

--- app/src/main/java/com/alperencitak/discover_movies_app/api/RetrofitInstance.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/api/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "https://api.themoviedb.org/"
+    private const val BASE_URL = "https://api.themoviedb.org/3/"
 
     val api: TMDBApiService by lazy {
         Retrofit.Builder()
--- app/src/main/java/com/alperencitak/discover_movies_app/api/TMDBApiService.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/api/TMDBApiService.kt
@@ -1,16 +1,42 @@
 package com.alperencitak.discover_movies_app.api
 
 import com.alperencitak.discover_movies_app.BuildConfig
+import com.alperencitak.discover_movies_app.model.GenreResponse
 import com.alperencitak.discover_movies_app.model.MovieResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface TMDBApiService {
-    @GET("3/discover/movie")
+
+    @GET("discover/movie")
     suspend fun getMovies(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
         @Query("language") language: String = "en-US",
         @Query("sort_by") sortBy: String = "popularity.desc",
         @Query("page") page: Int
     ): MovieResponse
+
+    @GET("movie/now_playing")
+    suspend fun getLatestMovies(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "en-US",
+        @Query("sort_by") sortBy: String = "primary_release_date.desc",
+        @Query("page") page: Int
+    ): MovieResponse
+
+    @GET("discover/movie")
+    suspend fun getMoviesByGenre(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("with_genres") genreId: Int,
+        @Query("language") language: String = "en-US",
+        @Query("sort_by") sortBy: String = "popularity.desc",
+        @Query("page") page: Int
+    ): MovieResponse
+
+    @GET("genre/movie/list")
+    suspend fun getMovieGenres(
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "en-US"
+    ): GenreResponse
+
 }
