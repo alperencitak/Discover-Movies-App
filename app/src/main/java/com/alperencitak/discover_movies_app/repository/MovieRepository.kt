e8c234ae2dcd9fff25213c038b0bf676d0964385
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-05 13:39:44 +0300

    Update Repository and ViewModel

--- app/src/main/java/com/alperencitak/discover_movies_app/repository/MovieRepository.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/repository/MovieRepository.kt
@@ -1,6 +1,7 @@
 package com.alperencitak.discover_movies_app.repository
 
 import com.alperencitak.discover_movies_app.api.TMDBApiService
+import com.alperencitak.discover_movies_app.model.Genre
 import com.alperencitak.discover_movies_app.model.Movie
 import javax.inject.Inject
 
@@ -9,5 +10,17 @@ class MovieRepository @Inject constructor(
 ) {
     suspend fun fetchMovies(page: Int): List<Movie> {
         return api.getMovies(page=page).results
+    }
+
+    suspend fun fetchLatestMovies(page: Int): List<Movie> {
+        return api.getLatestMovies(page=page).results
+    }
+
+    suspend fun fetchMoviesByGenre(page: Int, genreId: Int): List<Movie> {
+        return api.getMoviesByGenre(page=page, genreId=genreId).results
+    }
+
+    suspend fun fetchGenres(): List<Genre> {
+        return api.getMovieGenres().genres
     }
 }
--- app/src/main/java/com/alperencitak/discover_movies_app/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/viewmodel/MovieViewModel.kt
@@ -2,6 +2,7 @@ package com.alperencitak.discover_movies_app.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.alperencitak.discover_movies_app.model.Genre
 import com.alperencitak.discover_movies_app.model.Movie
 import com.alperencitak.discover_movies_app.repository.MovieRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,8 +19,11 @@ class MovieViewModel @Inject constructor(
     private val _movies = MutableStateFlow<List<Movie>>(emptyList())
     val movies: StateFlow<List<Movie>> = _movies
 
+    private val _genres = MutableStateFlow<List<Genre>>(emptyList())
+    val genres: StateFlow<List<Genre>> = _genres
+
     init {
-        getMovies(1)
+        getMovieGenres()
     }
 
     fun getMovies(page: Int) {
@@ -31,4 +35,35 @@ class MovieViewModel @Inject constructor(
             }
         }
     }
+
+    fun getLatestMovies(page: Int) {
+        viewModelScope.launch {
+            try {
+                _movies.value = repository.fetchLatestMovies(page=page)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun getMoviesByGenre(page: Int, genreId: Int){
+        viewModelScope.launch {
+            try {
+                _movies.value = repository.fetchMoviesByGenre(page, genreId)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun getMovieGenres(){
+        viewModelScope.launch {
+            try {
+                _genres.value = repository.fetchGenres()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
