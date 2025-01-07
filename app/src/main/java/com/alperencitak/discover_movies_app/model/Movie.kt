88c1355fd330071788de89c77218f36d96d7462f
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-07 15:47:53 +0300

    Movie model updated. (new rows)

--- app/src/main/java/com/alperencitak/discover_movies_app/model/Movie.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/model/Movie.kt
@@ -1,10 +1,14 @@
 package com.alperencitak.discover_movies_app.model
 
+import java.util.Date
+
 data class Movie(
     val id: Int,
     val title: String,
     val overview: String,
-    val poster_path: String
+    val poster_path: String,
+    val release_date: String,
+    val vote_average: Float
 ) {
     fun getFullPosterUrl(): String{
         return "https://image.tmdb.org/t/p/w500$poster_path"
