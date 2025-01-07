671c5f4562ed1e50578f64dec01b40a00136b2c8
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-07 23:22:26 +0300

    Movie detail screen was made

--- app/src/main/java/com/alperencitak/discover_movies_app/view/MovieDetailScreen.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/view/MovieDetailScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,12 +15,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -28,6 +33,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -36,8 +42,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
+import com.alperencitak.discover_movies_app.R
 import com.alperencitak.discover_movies_app.ui.theme.SoftBlack
+import com.alperencitak.discover_movies_app.ui.theme.SoftGray
 import com.alperencitak.discover_movies_app.ui.theme.SoftRed
+import com.alperencitak.discover_movies_app.ui.theme.SoftWhite
 import com.alperencitak.discover_movies_app.viewmodel.MovieViewModel
 
 @Composable
@@ -46,6 +55,10 @@ fun MovieDetailScreen(movieId: Int = 1){
     val context = LocalContext.current
     val movieAsStateFlow = movieViewModel.movie.collectAsState()
     movieViewModel.getMovie(movieId)
+
+    val nunito = FontFamily(
+        Font(R.font.nunito_black)
+    )
 
     Box(
         modifier = Modifier.fillMaxSize().background(SoftBlack)
@@ -62,7 +75,7 @@ fun MovieDetailScreen(movieId: Int = 1){
             ElevatedCard(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height((LocalConfiguration.current.screenHeightDp / 3).dp)
+                    .aspectRatio(2f / 3f)
                     .padding(bottom = 32.dp)
             ) {
                 Image(
@@ -73,15 +86,25 @@ fun MovieDetailScreen(movieId: Int = 1){
                 )
             }
             Row(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 32.dp, vertical = 32.dp),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 32.dp, vertical = 16.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Button(
                     onClick = {
-                        //TO-DO
-                    }
+
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = SoftRed
+                    ),
+                    shape = androidx.compose.foundation.shape.AbsoluteCutCornerShape(0)
                 ) {
-                    Text(text = "WATCH", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = SoftRed)
+                    Text(
+                        text = "WATCH",
+                        color = SoftBlack,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = nunito
+                    )
                 }
                 Button(
                     onClick = {
@@ -89,42 +112,75 @@ fun MovieDetailScreen(movieId: Int = 1){
                             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
                             context.startActivity(intent)
                         }
-                    }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = SoftRed
+                    ),
+                    shape = androidx.compose.foundation.shape.AbsoluteCutCornerShape(0)
                 ) {
-                    Text(text = "TRAILER", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = SoftRed)
+                    Text(
+                        text = "TRAILER",
+                        color = SoftBlack,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = nunito
+                    )
                 }
             }
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 32.dp, vertical = 32.dp)
+                    .padding(horizontal = 32.dp, vertical = 16.dp)
             ){
                 Text(
                     modifier = Modifier.fillMaxWidth(),
-                    color = SoftRed,
+                    color = SoftWhite,
+                    fontSize = 18.sp,
+                    fontFamily = nunito,
                     text = movie.movie.overview
                 )
             }
-            Box(
-                modifier = Modifier.fillMaxWidth().padding(32.dp)
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
             ){
                 Text(
                     color = SoftRed,
-                    text = "Actors"
+                    text = "Actors",
+                    fontSize = 20.sp,
+                    fontFamily = nunito
+                )
+                HorizontalDivider(
+                    color = Color.Gray,
+                    thickness = 1.dp,
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
                 )
                 LazyRow(
                     Modifier
                         .fillMaxWidth()
                         .height((LocalConfiguration.current.screenHeightDp / 3).dp)
-                        .padding(vertical = 16.dp)
+                        .padding(bottom = 16.dp)
                 ) {
                     items(movie.casts){ cast ->
                         ElevatedCard(
                             modifier = Modifier
                                 .fillMaxHeight()
-                                .aspectRatio(0.5f)
-                                .padding(horizontal = 4.dp, vertical = 8.dp)
+                                .width(200.dp)
+                                .padding(horizontal = 4.dp, vertical = 8.dp),
+                            colors = CardDefaults.elevatedCardColors(
+                                containerColor = SoftBlack
+                            )
                         ) {
+                            Text(
+                                color = SoftWhite,
+                                text = cast.name,
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = nunito,
+                                textAlign = TextAlign.Center,
+                                maxLines = 1,
+                                minLines = 1,
+                                fontSize = 18.sp,
+                                modifier = Modifier.fillMaxWidth().padding(4.dp)
+                            )
                             Image(
                                 painter = rememberAsyncImagePainter(
                                     model = cast.getFullPosterPath()
@@ -132,16 +188,6 @@ fun MovieDetailScreen(movieId: Int = 1){
                                 contentScale = ContentScale.Crop,
                                 contentDescription = "Movie Poster",
                                 modifier = Modifier.fillMaxSize()
-                            )
-                            Text(
-                                modifier = Modifier.fillMaxWidth(),
-                                color = Color.White,
-                                text = "\n${cast.name} : ${cast.character}",
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Bold,
-                                fontFamily = FontFamily.SansSerif,
-                                textAlign = TextAlign.Center,
-                                maxLines = 3
                             )
                         }
                     }
