209054976eb945551d715d940a3fa5906896b7cb
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-07 15:46:13 +0300

    Added movie details api in Tmdb Service

--- app/src/main/java/com/alperencitak/discover_movies_app/api/TMDBApiService.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/api/TMDBApiService.kt
@@ -1,9 +1,13 @@
 package com.alperencitak.discover_movies_app.api
 
 import com.alperencitak.discover_movies_app.BuildConfig
+import com.alperencitak.discover_movies_app.model.CreditsResponse
 import com.alperencitak.discover_movies_app.model.GenreResponse
+import com.alperencitak.discover_movies_app.model.Movie
 import com.alperencitak.discover_movies_app.model.MovieResponse
+import com.alperencitak.discover_movies_app.model.VideoResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TMDBApiService {
@@ -15,6 +19,26 @@ interface TMDBApiService {
         @Query("sort_by") sortBy: String = "popularity.desc",
         @Query("page") page: Int
     ): MovieResponse
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovie(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "en-US",
+    ): Movie
+
+    @GET("movie/{movie_id}/videos")
+    suspend fun getMovieVideos(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
+        @Query("language") language: String = "tr-TR"
+    ): VideoResponse
+
+    @GET("movie/{movie_id}/credits")
+    suspend fun getMovieCredits(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY
+    ): CreditsResponse
 
     @GET("movie/now_playing")
     suspend fun getLatestMovies(
