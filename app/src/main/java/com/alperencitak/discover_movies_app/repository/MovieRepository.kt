717e82de062e9a50772fba4b17af066836546efd
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-07 15:48:47 +0300

    Fetch movie func written in repository

--- app/src/main/java/com/alperencitak/discover_movies_app/repository/MovieRepository.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/repository/MovieRepository.kt
@@ -3,6 +3,7 @@ package com.alperencitak.discover_movies_app.repository
 import com.alperencitak.discover_movies_app.api.TMDBApiService
 import com.alperencitak.discover_movies_app.model.Genre
 import com.alperencitak.discover_movies_app.model.Movie
+import com.alperencitak.discover_movies_app.model.MovieWithDetails
 import javax.inject.Inject
 
 class MovieRepository @Inject constructor(
@@ -10,6 +11,16 @@ class MovieRepository @Inject constructor(
 ) {
     suspend fun fetchMovies(page: Int): List<Movie> {
         return api.getMovies(page=page).results
+    }
+
+    suspend fun fetchMovie(movieId: Int): MovieWithDetails{
+        val movie = MovieWithDetails(
+            movie=api.getMovie(movieId),
+            videos=api.getMovieVideos(movieId).results,
+            casts=api.getMovieCredits(movieId).cast,
+            crews=api.getMovieCredits(movieId).crew
+        )
+        return movie
     }
 
     suspend fun fetchLatestMovies(page: Int): List<Movie> {
@@ -23,4 +34,5 @@ class MovieRepository @Inject constructor(
     suspend fun fetchGenres(): List<Genre> {
         return api.getMovieGenres().genres
     }
+
 }
