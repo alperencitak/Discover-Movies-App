cab388d054bb0b6c42d70ccbbe26552213edc8e9
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-17 15:23:12 +0300

    added search icon and put trends of the day in the background

--- app/src/main/java/com/alperencitak/discover_movies_app/view/MovieListScreen.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/view/MovieListScreen.kt
@@ -2,23 +2,19 @@ package com.alperencitak.discover_movies_app.view
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.ElevatedCard
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -29,17 +25,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import coil.compose.rememberAsyncImagePainter
-import com.alperencitak.discover_movies_app.model.Movie
 import com.alperencitak.discover_movies_app.ui.theme.SoftBlack
+import com.alperencitak.discover_movies_app.ui.theme.SoftRed
 import com.alperencitak.discover_movies_app.utils.CircularLoadingScreen
 import com.alperencitak.discover_movies_app.utils.HeadMovieItem
 import com.alperencitak.discover_movies_app.utils.MovieItem
@@ -50,13 +44,14 @@ import kotlinx.coroutines.delay
 fun MovieListScreen(navController: NavHostController) {
     val movieViewModel: MovieViewModel = hiltViewModel()
     val movies by movieViewModel.movies.collectAsState()
+    val trendingMoviesToday by movieViewModel.trendingMovies.collectAsState()
     var currentImageUrl by remember {
         mutableStateOf<String?>(null)
     }
     var currentPage by remember { mutableIntStateOf(1) }
     var isLoadingMore by remember { mutableStateOf(false) }
 
-
+    movieViewModel.getTrendingMovies(1, "day")
     LaunchedEffect(currentPage) {
         if(!isLoadingMore){
             isLoadingMore = true
@@ -75,10 +70,20 @@ fun MovieListScreen(navController: NavHostController) {
         LaunchedEffect(Unit) {
             while (true) {
                 delay(5000)
-                currentImageUrl = movies.take(15).shuffled().first().getFullPosterUrl()
+                currentImageUrl = trendingMoviesToday.take(10).shuffled().first().getFullPosterUrl()
             }
         }
-
+        Box(
+            modifier = Modifier.fillMaxWidth().zIndex(2f),
+            contentAlignment = Alignment.TopEnd
+        ){
+            Icon(
+                Icons.Default.Search,
+                contentDescription = "Search Icon",
+                tint = SoftRed,
+                modifier = Modifier.align(Alignment.CenterEnd).padding(top = 64.dp, end = 32.dp).scale(1.5f)
+            )
+        }
         Box(
             modifier = Modifier
                 .fillMaxSize()
