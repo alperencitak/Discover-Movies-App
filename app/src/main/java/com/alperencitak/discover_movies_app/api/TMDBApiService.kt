a7b6ed3821f927a8619727c30067425590769aba
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-19 22:26:05 +0300

    Api language has been set

--- app/src/main/java/com/alperencitak/discover_movies_app/api/TMDBApiService.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/api/TMDBApiService.kt
@@ -1,6 +1,8 @@
 package com.alperencitak.discover_movies_app.api
 
+import androidx.compose.ui.res.stringResource
 import com.alperencitak.discover_movies_app.BuildConfig
+import com.alperencitak.discover_movies_app.R
 import com.alperencitak.discover_movies_app.model.CreditsResponse
 import com.alperencitak.discover_movies_app.model.GenreResponse
 import com.alperencitak.discover_movies_app.model.Movie
@@ -15,7 +17,7 @@ interface TMDBApiService {
     @GET("discover/movie")
     suspend fun getMovies(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
-        @Query("language") language: String = "en-US",
+        @Query("language") language: String,
         @Query("sort_by") sortBy: String = "popularity.desc",
         @Query("page") page: Int
     ): MovieResponse
@@ -24,14 +26,14 @@ interface TMDBApiService {
     suspend fun getMovie(
         @Path("movie_id") movieId: Int,
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
-        @Query("language") language: String = "en-US",
+        @Query("language") language: String,
     ): Movie
 
     @GET("movie/{movie_id}/videos")
     suspend fun getMovieVideos(
         @Path("movie_id") movieId: Int,
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
-        @Query("language") language: String = "tr-TR"
+        @Query("language") language: String
     ): VideoResponse
 
     @GET("movie/{movie_id}/credits")
@@ -43,7 +45,7 @@ interface TMDBApiService {
     @GET("movie/now_playing")
     suspend fun getLatestMovies(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
-        @Query("language") language: String = "en-US",
+        @Query("language") language: String,
         @Query("sort_by") sortBy: String = "primary_release_date.desc",
         @Query("page") page: Int
     ): MovieResponse
@@ -51,7 +53,7 @@ interface TMDBApiService {
     @GET("movie/top_rated")
     suspend fun getTopRatedMovies(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
-        @Query("language") language: String = "en-US",
+        @Query("language") language: String,
         @Query("page") page: Int
     ): MovieResponse
 
@@ -59,7 +61,7 @@ interface TMDBApiService {
     suspend fun getTrendingMoviesToday(
         @Path("time_window") timeWindow: String,
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
-        @Query("language") language: String = "en-US",
+        @Query("language") language: String,
         @Query("page") page: Int
     ): MovieResponse
 
@@ -67,7 +69,7 @@ interface TMDBApiService {
     suspend fun searchMovie(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
         @Query("query") query: String,
-        @Query("language") language: String = "en-US",
+        @Query("language") language: String,
         @Query("page") page: Int
     ): MovieResponse
 
@@ -75,7 +77,7 @@ interface TMDBApiService {
     suspend fun getMoviesByGenre(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
         @Query("with_genres") genreId: Int,
-        @Query("language") language: String = "en-US",
+        @Query("language") language: String,
         @Query("sort_by") sortBy: String = "popularity.desc",
         @Query("page") page: Int
     ): MovieResponse
@@ -83,7 +85,7 @@ interface TMDBApiService {
     @GET("genre/movie/list")
     suspend fun getMovieGenres(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
-        @Query("language") language: String = "en-US"
+        @Query("language") language: String
     ): GenreResponse
 
 }
