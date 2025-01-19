39e619c34d086efcd169a4eb474da349581b321a
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-19 13:02:55 +0300

    Poster path made may be null

--- app/src/main/java/com/alperencitak/discover_movies_app/model/Movie.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/model/Movie.kt
@@ -6,7 +6,7 @@ data class Movie(
     val id: Int,
     val title: String,
     val overview: String,
-    val poster_path: String,
+    val poster_path: String?,
     val release_date: String,
     val vote_average: Float,
     val genres: List<Genre>
