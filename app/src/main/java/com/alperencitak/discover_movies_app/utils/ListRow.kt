890bbf03be095ae1c3d4036cf24333dc7543f9d6
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-05-01 12:35:21 +0300

    Fix minor bug

--- app/src/main/java/com/alperencitak/discover_movies_app/utils/ListRow.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/utils/ListRow.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -79,7 +80,10 @@ fun ListRow(
                         .padding(horizontal = 4.dp, vertical = 8.dp)
                         .clickable {
                             onClick(movie.id)
-                        }
+                        },
+                    colors = CardDefaults.elevatedCardColors(
+                        containerColor = SoftBlack
+                    )
                 ) {
                     Image(
                         painter = rememberAsyncImagePainter(model = movie.getFullPosterUrl()),
