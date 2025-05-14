6bbd11fe8338ef979d6986552e4ea939d3054897
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-05-14 15:50:41 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.alperencitak.discover_movies_app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
