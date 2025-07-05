09743101d0b04cd40382b1f9c3c5b65bfbee967a
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-07-05 12:53:57 +0300

    Update AppModule.kt

--- app/src/main/java/com/alperencitak/discover_movies_app/AppModule.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/AppModule.kt
@@ -2,24 +2,55 @@ package com.alperencitak.discover_movies_app
 
 import android.app.Application
 import android.content.Context
-import com.alperencitak.discover_movies_app.api.RetrofitInstance
-import com.alperencitak.discover_movies_app.api.TMDBApiService
+import com.alperencitak.discover_movies_app.data.remote.MoviesApi
+import com.alperencitak.discover_movies_app.data.repository.MoviesRepositoryImpl
+import com.alperencitak.discover_movies_app.domain.repository.MoviesRepository
+import com.alperencitak.discover_movies_app.domain.usecases.movies.GetMovies
+import com.alperencitak.discover_movies_app.domain.usecases.movies.MoviesUseCases
+import com.alperencitak.discover_movies_app.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule {
+object AppModule {
 
     @Provides
-    fun provideContext(app: Application): Context {
-        return app.applicationContext
+    @Singleton
+    fun provideContext(
+        app: Application
+    ): Context = app.applicationContext
+
+    @Provides
+    @Singleton
+    fun provideMoviesApi(): MoviesApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MoviesApi::class.java)
     }
 
     @Provides
-    fun provideTMDBApiService(): TMDBApiService {
-        return RetrofitInstance.api
+    @Singleton
+    fun provideMoviesRepository(
+        moviesApi: MoviesApi,
+        context: Context
+    ): MoviesRepository = MoviesRepositoryImpl(moviesApi = moviesApi, context = context)
+
+    @Provides
+    @Singleton
+    fun provideMoviesUseCases(
+        moviesRepository: MoviesRepository
+    ): MoviesUseCases{
+        return MoviesUseCases(
+            getMovies = GetMovies(moviesRepository)
+        )
     }
+
 }
