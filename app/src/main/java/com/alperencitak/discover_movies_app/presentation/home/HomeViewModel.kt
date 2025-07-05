d712fa9b908585b9bda551ea17df27e763d52af3
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-07-05 12:53:49 +0300

    Create HomeViewModel.kt

---
+++ app/src/main/java/com/alperencitak/discover_movies_app/presentation/home/HomeViewModel.kt
@@ -0,0 +1,17 @@
+package com.alperencitak.discover_movies_app.presentation.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.alperencitak.discover_movies_app.domain.usecases.movies.MoviesUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val moviesUseCases: MoviesUseCases
+): ViewModel() {
+
+    val movies = moviesUseCases.getMovies().cachedIn(viewModelScope)
+
+}
