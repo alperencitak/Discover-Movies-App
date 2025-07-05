ed870f7d3a673359f9a8d426ec36b3a7cf369d83
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-07-05 12:54:00 +0300

    Update MainActivity.kt

--- app/src/main/java/com/alperencitak/discover_movies_app/MainActivity.kt
+++ app/src/main/java/com/alperencitak/discover_movies_app/MainActivity.kt
@@ -3,49 +3,62 @@ package com.alperencitak.discover_movies_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.alperencitak.discover_movies_app.presentation.navigator.MovieNavigator
 import com.alperencitak.discover_movies_app.ui.theme.DiscoverMoviesAppTheme
-import com.alperencitak.discover_movies_app.ui.theme.SoftBlack
 import com.alperencitak.discover_movies_app.ui.theme.SoftDarkBlue
 import com.alperencitak.discover_movies_app.ui.theme.SoftRed
-import com.alperencitak.discover_movies_app.view.NavScreen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             DiscoverMoviesAppTheme {
-                ScaffoldWithNavBar()
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+                    systemController.setNavigationBarColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    MovieNavigator()
+                }
             }
         }
     }
@@ -130,6 +143,7 @@ fun ScaffoldWithNavBar(){
             }
         }
     ){ paddingValues ->
-        NavScreen(navController, paddingValues)
+        paddingValues
+//        NavScreen(navController, paddingValues)
     }
 }
